94ff926c06ded08ee13e0d33660e517bca687913
DHANUSH KUMAR K <dhanushkumark29@gmail.com>
2026-02-16 18:25:01 +0530

    redeploy fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
-export const dynamic = 'force-dynamic'
 
 'use client'
+
+export const dynamic = 'force-dynamic'
 
 import { supabase } from '@/lib/supabase'
 import { useEffect, useState } from 'react'
